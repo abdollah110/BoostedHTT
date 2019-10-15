c53a569e20a7c57e27bc6d771d76baff56b35465
abdollah <smohamadi@gmail.com>
2019-10-15 13:49:24 -0500

    hadd

--- Skim/hadd_skim.py
+++ Skim/hadd_skim.py
@@ -4,9 +4,9 @@ files='/eos/uscms/store/user/abdollah/SkimBoost/mm/v4'
 HaddLoc=files+'_Hadd'
 
 for gl in glob.glob('/eos/uscms/store/user/tmitchel/BoostedH/An2017/MC/*/*'):
-    Name=gl.rpartition('/')[-1].replace('crab_','')
+    name=gl.rpartition('/')[-1].replace('crab_','')
 #    os.system('hadd $s/%s.root    $s/%s*.root'%(HaddLoc,name,files,name))
-    print 'hadd $s/%s.root    $s/%s*.root'%(HaddLoc,name,files,name)
+    print 'hadd %s/%s.root    %s/%s*.root'%(HaddLoc,name,files,name)
     
     
 
