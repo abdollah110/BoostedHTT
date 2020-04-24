7102082167c9446919295f3abd59a6a86d4c9b8e
abdollah <smohamadi@gmail.com>
2020-04-24 09:10:15 -0500

    update the template run.sh

--- SVFitSubmitter/Template_Submit_sv_run.sh
+++ SVFitSubmitter/Template_Submit_sv_run.sh
@@ -6,11 +6,9 @@ PROCESS=$2
 RUNPATH=$3
 OUTPATH=$4
 
-source /cvmfs/cms.cern.ch/cmsset_default.sh
-
-
 echo "PROCESS: $PROCESS"
 
+source /cvmfs/cms.cern.ch/cmsset_default.sh
 setenv SCRAM_ARCH slc6_amd64_gcc630
 scramv1 project CMSSW_9_4_13
 cd CMSSW_9_4_13/src
@@ -23,29 +21,14 @@ eval `scramv1 runtime -sh`
 chmod a+x FastMTT
 ls
 
-#
-#
-#xrdcp root://cmseos.fnal.gov//store/user/abdollah/CMSSW_9_4_4.tar.gz   .
-#tar -xf CMSSW_9_4_4.tar.gz
-#rm CMSSW_9_4_4.tar.gz
-#cd CMSSW_9_4_4/src/SubmitSVFit/skim
-#scramv1 b ProjectRename
-#eval `scramv1 runtime -sh`
-#ls
-#
-
-echo "this is the process number " $PROCESS
-
 ############  Here is where the Skimmer is running     ############
-
-sed -n "${PROCESS}p"  < InputSamples_sv.txt > out.out
-FullDataSetName=$(head -n 1 out.out)
-
+#find the nth line of the InputSamples_sv.txt where n is the PROCESS number
 # more info here \https://unix.stackexchange.com/questions/147560/explain-this-bash-script-echo-1
-ShortName=${FullDataSetName##*/}
+FullSampleName=`sed -n "$((PROCESS+1))p"  < InputSamples_sv.txt`
+ShortName=${FullSampleName##*/}
 
 outName="svfited_"$ShortName
-input="root://cmseos.fnal.gov/"$FullDataSetName
+input="root://cmseos.fnal.gov/"$FullSampleName
 
 echo "  input is " $input
 echo "  output is " $outName
