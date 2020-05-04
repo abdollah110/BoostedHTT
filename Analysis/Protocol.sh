e97a3f0d31e75f651c09bbd569abcb4e11c7f167
abdollah <smohamadi@gmail.com>
2020-05-04 10:16:48 -0500

    add protocol

--- Analysis/Protocol.sh
+++ Analysis/Protocol.sh
@@ -1,7 +1,17 @@
-python automate_analysis.py -e BoostedHTT2017_mt.exe -p /eos/uscms/store/user/abdollah/SkimBoost/mt/v6_Hadd/ -P XXX -o newtest_data_includeHiggs
-./build process_trees_Tot.cc out
-./out -d TestOut2 -t mutau_tree --suf ZMass -y 2017 -o vis_mass
+##Running analysis
+
+#complie the code
+./Make.sh BoostedHTT2017_mt.cc
+# Run the executable
+python automate_analysis.py -e BoostedHTT2017_mt.exe -p /eos/uscms/store/user/abdollah/SkimBoost/mt/v6_Hadd/ -P muTau2017 -o newtest_data_includeHiggs
+
+#To make control plots
+# first compile the .cc file
+./build process_trees_lt.cc process_trees_lt
+# Then run the executable
+./process_trees_lt -d TestOut2 -t mutau_tree --suf ZMass -y 2017 -o vis_mass
+# Make plots
 python autoplot.py -i Output/templates/mt2017_ZMass.root -p mass -y 2017 -c mt
 
-python xrun.py -i NewAnalysis_v2 -y 2017
-python autoplot.py -i Output/templates/mt2017_vis_mass.root -y 2017 -p test -c mt
+#python xrun.py -i NewAnalysis_v2 -y 2017
+#python autoplot.py -i Output/templates/mt2017_m_sv.root -y 2017 -p test -c mt
