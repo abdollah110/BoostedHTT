26442a9411ce3961f02725fbdf8f322fe9470401
abdollah110 <smohamadi@gmail.com>
2020-11-18 11:24:49 -0600

    add rename

---
+++ Skim/_rename.sh
@@ -0,0 +1,12 @@
+dir=$1
+
+hadd $dir/Data.root $dir/SingleMuon*root
+hadd $dir/Tbar-tchan.root          $dir/Tbar-tchan_*.root
+hadd $dir/WZ1l1nu2q.root          $dir/WZ1l1nu2q_*.root
+hadd $dir/T-tW.root          $dir/T-tW_*root
+mv $dir/GluGluHToTauTau_M125.root   $dir/ggH125.root
+mv $dir/VBFHToTauTau_M125.root   $dir/qqH125.root
+mv $dir/WminusHToTauTau_M125.root   $dir/WMinusH125.root
+mv $dir/WplusHToTauTau_M125.root   $dir/WPlusH125.root
+mv $dir/ZHToTauTau_M125.root   $dir/ZH125.root
+
