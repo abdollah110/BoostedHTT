353f7e2e4fb45a74973e61a788cde775cba41b64
abdollah110 <smohamadi@gmail.com>
2020-12-15 21:50:58 -0600

    add pu data files

--- Analysis/BoostedHTT2017_mt.cc
+++ Analysis/BoostedHTT2017_mt.cc
@@ -110,9 +110,9 @@ int main(int argc, char* argv[]) {
     float BJetPtCut=20;
     
         float DeepCSVCut=   1000   ;                  //  loose  https://twiki.cern.ch/twiki/bin/viewauth/CMS/BtagRecommendation94X
-    if (year== 2016) DeepCSVCut =    0.2217 ;
-    if (year== 2017) DeepCSVCut =    0.1522 ;
-    if (year== 2018) DeepCSVCut =   0.1241  ;
+    if (year== 2016) DeepCSVCut =     0.8953  ;
+    if (year== 2017) DeepCSVCut =     0.8001  ;
+    if (year== 2018) DeepCSVCut =    0.7527   ;
 
     
     float LeptonIsoCut=0.15;
