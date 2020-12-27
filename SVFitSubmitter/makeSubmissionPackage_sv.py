b8828b5588b2193cd65f6e2437bd3937f1a518c7
abdollah110 <smohamadi@gmail.com>
2020-12-27 16:28:33 -0600

    update codes

--- SVFitSubmitter/makeSubmissionPackage_sv.py
+++ SVFitSubmitter/makeSubmissionPackage_sv.py
@@ -9,11 +9,13 @@ import subprocess as sp
 
 parser = argparse.ArgumentParser(description='create sumission files from the existing templates')
 parser.add_argument('-x','--executable',action='store',default='FastMTT',dest='exe', help='the name of the executable')
-parser.add_argument('-i','--input',action='store',default='InputSamples_sv.txt',dest='InputSample', help='the name of the INpitSamples')
+#parser.add_argument('-i','--input',action='store',default='InputSamples_sv.txt',dest='InputSample', help='the name of the INpitSamples')
 parser.add_argument('-n','--name',action='store',default='_tt_v1',dest='Name', help='suffix to the script names')
-parser.add_argument('-o','--output',action='store',default='/store/user/abdollah/NewSkimBoost/mt/v2',dest='OutPut', help='Location of the output file')
+parser.add_argument('-i','--input',action='store',default='/store/user/abdollah/NewSkimBoost/mt/v2',dest='Input', help='Location of the Input file')
 args = parser.parse_args()
 
+
+os.system("find {} > InputSamples_sv.txt".format(args.Input))
 
 ## template for Submit_run
 Out_run=open('Submit_run_%s.sh'%args.Name,'w')
@@ -23,8 +25,8 @@ for line in run_file.readlines():
     line=line.replace('abdollah',os.environ['LOGNAME'])
     line=line.replace('FastMTT',args.exe)
     line=line.replace('TarFile','Files_%s'%args.Name)
-    line=line.replace('OUTPUTFILE_LOCATION',args.OutPut)
-    line=line.replace('InputSamples_sv.txt',args.InputSample)
+    line=line.replace('OUTPUTFILE_LOCATION','{}_fast'.format(args.Input).replace('/eos/uscms/','').replace('/_','_'))
+#    line=line.replace('InputSamples_sv.txt',args.InputSample)
 
 
     Out_run.write(line)
@@ -32,7 +34,7 @@ Out_run.close()
 
 
 ## Count number of files
-input_File=open(args.InputSample,'r')
+input_File=open('InputSamples_sv.txt','r')
 count=0
 for line in input_File.readlines():
     if len(line.strip()) == 0 : continue
@@ -55,16 +57,16 @@ Out_batch.close()
 
 ## copy necessary files in the /eos are
 logName=os.environ['LOGNAME']
-#exeLocation = sp.getoutput('which %s'%args.exe)
+#exeLocation = sp.getInput('which %s'%args.exe)
 exeLocation = os.popen('which %s'%args.exe).read().replace('\n','')
 #exeLocation=os.system('which %s'%args.exe)
 print 'exeLocation= ', exeLocation
 os.system('rm -r Files_%s'%args.Name)
 os.system('mkdir Files_%s'%args.Name)
-os.system('cp Submit_run_%s.sh Files_%s/.'%(args.Name,args.Name))
-os.system('cp Submit_batch_%s.jdl Files_%s/.'%(args.Name,args.Name))
+os.system('mv Submit_run_%s.sh Files_%s/.'%(args.Name,args.Name))
+os.system('mv Submit_batch_%s.jdl Files_%s/.'%(args.Name,args.Name))
 os.system('cp %s Files_%s/.'%(exeLocation,args.Name))
-os.system('cp %s Files_%s/.'%(args.InputSample,args.Name))
+os.system('mv InputSamples_sv.txt Files_%s/.'%(args.Name))
 # needed libraries for FastMTT
 os.system('cp %s Files_%s/.'%("$CMSSW_BASE/lib/*/libTauAnalysisSVfitTF.so" ,args.Name))
 os.system('cp %s Files_%s/.'%("$CMSSW_BASE/lib/*/libHTT-utilitiesRecoilCorrections.so" ,args.Name))
@@ -75,12 +77,13 @@ os.system('tar -zcp -f Files_%s.tar.gz  Files_%s'%(args.Name,args.Name))
 
 os.system('xrdcp -f Files_%s.tar.gz root://cmseos.fnal.gov://store/user/${LOGNAME}/.'%args.Name)
     
-    
+os.system('mv Files_%s.tar.gz Files_%s/.'%(args.Name,args.Name))
+
 ## Print the condos submit command
 print 'total number of jobs to be submitted will be %d'%(count)
 print 'Just run the following command:'
 print '................................'
-print 'condor_submit Submit_batch_%s.jdl'%args.Name
+print 'cd Files_%s/; condor_submit Submit_batch_%s.jdl; cd -'%(args.Name,args.Name)
 print '................................'
 
 
