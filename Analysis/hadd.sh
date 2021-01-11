6efc0ece675b3ab876afb91f6b64a40f85ac33cb
abdollah110 <smohamadi@gmail.com>
2021-01-10 22:56:06 -0600

    update hadd

--- Analysis/hadd.sh
+++ Analysis/hadd.sh
@@ -14,16 +14,15 @@ do
     hadd   -f NN${SYS}/W${SYS}.root    ${SYS}/*_W_*.root
     hadd   -f NN${SYS}/VV${SYS}.root    ${SYS}/*_VV_*.root
     hadd   -f NN${SYS}/TT${SYS}.root    ${SYS}/TT*.root
-    hadd   -f NN${SYS}/ZLL${SYS}.root    ${SYS}/*_ZLL_*.root
+#    hadd   -f NN${SYS}/ZLL${SYS}.root    ${SYS}/*_ZLL_*.root
     hadd   -f NN${SYS}/ZTT${SYS}.root    ${SYS}/*_ZTT_*
-    hadd   -f NN${SYS}/ZJ${SYS}.root    ${SYS}/*_ZJ_*
+#    hadd   -f NN${SYS}/ZJ${SYS}.root    ${SYS}/*_ZJ_*
     hadd   -f NN${SYS}/JJH125${SYS}.root    ${SYS}/*JJH*
 #    hadd   -f NN${SYS}/WH125${SYS}.root    ${SYS}/*WMinusH125_* ${SYS}/*WPlusH125_*
 #    hadd   -f NN${SYS}/ZH125${SYS}.root    ${SYS}/*ZH125_*
 #    hadd   -f NN${SYS}/ggH125${SYS}.root    ${SYS}/*ggH125*
 #    hadd   -f NN${SYS}/qqH125${SYS}.root    ${SYS}/*qqH125*
     hadd  -f NN${SYS}/H125${SYS}.root   ${SYS}/*WMinusH125_* ${SYS}/*WPlusH125_*  ${SYS}/*ZH125_*  ${SYS}/*ggH125*  ${SYS}/*qqH125*
-    hadd   -f NN${SYS}/H125${SYS}.root    ${SYS}/*WMinusH125_* ${SYS}/*WPlusH125_* ${SYS}/*ZH125_*  ${SYS}/*ggH125*  ${SYS}/*qqH125*
 
 
 done
