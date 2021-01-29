a47df3122dfe0b16a0c4ae9cf711313d697c57d0
abdollah110 <smohamadi@gmail.com>
2021-01-29 07:51:50 -0600

    btag

--- Analysis/BoostedHTT_mt.cc
+++ Analysis/BoostedHTT_mt.cc
@@ -327,8 +327,8 @@ int main(int argc, char* argv[]) {
             
             int leadCSV= leadingCSV();
             int subLeadCSV= subLeadingCSV(leadCSV);
-            cout<< "leading csv = "<< leadCSV <<" cvs= "<< jetDeepCSVTags_b->at(leadCSV) << "  pt= " << jetPt->at(leadCSV);
-            cout<< "subLeading csv = "<< subLeadCSV <<" cvs= "<< jetDeepCSVTags_b->at(subLeadCSV) << "  pt= " << jetPt->at(subLeadCSV);
+            cout<< "leading csv = "<< leadCSV <<" cvs= "<< jetDeepCSVTags_b->at(leadCSV) << "  pt= " << jetPt->at(leadCSV)<<"\n";
+            cout<< "subLeading csv = "<< subLeadCSV <<" cvs= "<< jetDeepCSVTags_b->at(subLeadCSV) << "  pt= " << jetPt->at(subLeadCSV)<<"\n";
             
 //            if (year==2016){
 //
