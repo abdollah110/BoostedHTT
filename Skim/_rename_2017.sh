331aeaf02b213ccb5c7d5d2051ecb648777b5021
abdollah110 <smohamadi@gmail.com>
2021-02-06 07:20:47 -0600

    add TT2l2nu

--- Skim/_rename_2017.sh
+++ Skim/_rename_2017.sh
@@ -5,6 +5,7 @@ hadd $dir/Tbar-tchan.root          $dir/Tbar-tchan_*.root
 hadd $dir/WZ1l1nu2q.root          $dir/WZ1l1nu2q_*.root
 hadd $dir/T-tW.root          $dir/T-tW_*root
 hadd $dir/ggH125.root  $dir/GluGluHToTauTau*.root
+hadd $dir/TTTo2L2Nu.root  $dir/TTTo2L2Nu*.root
 mv $dir/VBFHToTauTau_M125.root   $dir/qqH125.root
 mv $dir/WminusHToTauTau_M125.root   $dir/WMinusH125.root
 mv $dir/WplusHToTauTau_M125.root   $dir/WPlusH125.root
