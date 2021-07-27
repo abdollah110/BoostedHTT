59c26dcf7c39cea534f4db87a3d4d40ba4554d80
abdollah110 <smohamadi@gmail.com>
2021-07-27 16:06:10 -0500

    check  theory

--- Analysis/DiffMeasure_Theory.cc
+++ Analysis/DiffMeasure_Theory.cc
@@ -27,7 +27,20 @@ int main(int argc, char* argv[]) {
     else if (path.find("2018") != string::npos) year_str = "2018";
     else cout<<"Which year are you looking for \n\n";
     cout<<"%%%% Note: you are running on  "<< year_str <<"%%%%\n";
-    
+
+
+
+    std::string channel_tree;
+    if (path.find("_mt") != string::npos) channel_tree = "mutau_tree";
+    else if (path.find("_et") != string::npos) channel_tree = "etau_tree";
+    else if (path.find("_em") != string::npos) channel_tree = "emu_tree";
+    else if (path.find("_me") != string::npos) channel_tree = "mue_tree";
+    else if (path.find("_tt") != string::npos) channel_tree = "tautau_tree";
+    else cout<<"Which channel are you looking for \n\n";
+    cout<<"%%%% Note: you are running on  "<< channel_tree <<"%%%%\n";
+
+
+
     stringstream yearstream(year_str);
     int year=0;
     yearstream >> year;
@@ -56,7 +69,7 @@ int main(int argc, char* argv[]) {
     auto InputFile = TFile::Open(fname.c_str());
     std::cout << "Loading Ntuple..." << std::endl;
     TTree *  Run_Tree;
-    Run_Tree= Xttree(InputFile,"mutau_tree");
+    Run_Tree= Xttree(InputFile,channel_tree);
     //    Run_Tree= Xttree(InputFile,"EventTree");
     
     
@@ -66,38 +79,8 @@ int main(int argc, char* argv[]) {
     
     myMap1 = new std::map<std::string, TH1F*>();
     myMap2 = new map<string, TH2F*>();
-    
-    
-    TTree * outTr=  new TTree("mutau_tree","mutau_tree");
     TH1F * higpt=new TH1F("hig","hig",sizeof(xbin)/sizeof(xbin[0]) - 1, &xbin[0]);
-    
-    //########################################
-    // Muon Id, Iso, Trigger and Tracker Eff files
-    //########################################
-    TH2F** HistoMuId=FuncHistMuId(year);
-    TH2F** HistoMuIso=FuncHistMuIso(year);
-    TH2F** HistoMuTrg50=FuncHistMuTrigger_50(year);
-    TH2F** HistoMuTrg27=FuncHistMuTrigger_27(year);
-    //    TGraphAsymmErrors * HistoMuTrack=FuncHistMuTrack();
-    
-    //########################################
-    // Pileup files
-    //########################################
-    
-    TH1F *  HistoPUData =HistPUData(year_str);
-    TH1F * HistoPUMC = new TH1F();
-    if (! (fname.find("Data") != string::npos || fname.find("Run") != string::npos ))
-        HistoPUMC=HistPUMC(InputFile);
-    
-    //    // H->tau tau scale factors
-    //    TFile htt_sf_file("data/htt_scalefactors_2017_v2.root");
-    //    RooWorkspace *htt_sf = reinterpret_cast<RooWorkspace*>(htt_sf_file.Get("w"));
-    //    htt_sf_file.Close();
-    
-    // Z-pT reweighting
-    //        TFile *zpt_file = new TFile("data/zpt_weights_2016_BtoH.root");
-    TFile *zpt_file = new TFile(("data/zmm_2d"+year_str+".root").c_str());
-    auto zpt_hist = reinterpret_cast<TH2F*>(zpt_file->Get("Ratio2D"));    
+
     
     //###############################################################################################
     // Parameters
@@ -107,57 +90,6 @@ int main(int argc, char* argv[]) {
     float JetPtCut=30;
     float BJetPtCut=20;
     
-    float DeepCSVCut= 1   ;                  //  Tight  https://twiki.cern.ch/twiki/bin/viewauth/CMS/BtagRecommendation94X
-    if (year== 2016) DeepCSVCut =     0.8953  ;
-    if (year== 2017) DeepCSVCut =     0.8001  ;
-    if (year== 2018) DeepCSVCut =    0.7527   ;
-    
-    
-    float LeptonIsoCut=0.30;
-    bool debug= false;
-    float MuIdCorrection=1;
-    float MuIsoCorrection=1;
-    float MuTrgCorrection=1;
-    float LepCorrection=1;
-    float LumiWeight = 1;
-    float PUWeight = 1;
-    float zmasspt_weight=1;
-    float zmasspt_weight_err=0;
-    float zmasspt_weight_nom=1;
-    float WBosonKFactor=1;
-    float preFireWeight=1;
-    float bjetsWeightOnMC=1;
-    float ttbar_rwt=1;
-    
-    float lepPt_=-10;
-    float taupt_=-10;
-    float vis_mass=-10;
-    float LeadJetPt = -10;
-    float dR_Z_jet=-10;
-    bool OS,SS,lep1IsoPass,lep2IsoPassL,lep2IsoPassV,lep2IsoPassM,lep2IsoPassT;
-    float tmass,ht,st,Met,FullWeight, dR_lep_lep, Metphi,BoostedTauRawIso, higgs_pT, higgs_m, m_sv_, wtnom_zpt_weight, gen_higgs_pT;
-    int nbjet;
-    
-    outTr->Branch("evtwt",&FullWeight,"evtwt/F");
-    outTr->Branch("zmasspt_weight",&zmasspt_weight,"zmasspt_weight/F");
-    outTr->Branch("lep1Pt",&lepPt_,"lep1Pt/F");
-    outTr->Branch("lep2Pt",&taupt_,"lep2Pt/F");
-    outTr->Branch("OS",&OS,"OS/O");
-    outTr->Branch("SS",&SS,"SS/O");
-    outTr->Branch("vis_mass",&vis_mass,"vis_mass/F");
-    outTr->Branch("tmass",&tmass,"tmass/F");
-    outTr->Branch("ht",&ht,"ht/F");
-    outTr->Branch("st",&st,"st/F");
-    outTr->Branch("Met",&Met,"Met/F");
-    outTr->Branch("LeadJetPt",&LeadJetPt,"LeadJetPt/F");
-    outTr->Branch("dR_lep_lep",&dR_lep_lep,"dR_lep_lep/F");
-    outTr->Branch("higgs_pT",&higgs_pT,"higgs_pT/F");
-    outTr->Branch("higgs_m",&higgs_m,"higgs_m/F");
-    outTr->Branch("m_sv",&m_sv_,"m_sv/F");
-    outTr->Branch("dR_Z_jet",&dR_Z_jet,"dR_Z_jet/F");
-    outTr->Branch("gen_higgs_pT",&gen_higgs_pT,"gen_higgs_pT/F");
-    
-    
     
     Int_t nentries_wtn = (Int_t) Run_Tree->GetEntries();
     cout<<"nentries_wtn===="<<nentries_wtn<<"\n";
@@ -166,92 +98,57 @@ int main(int argc, char* argv[]) {
         Run_Tree->GetEntry(i);
         if (i % 1000 == 0) fprintf(stdout, "\r  Processed events: %8d of %8d ", i, nentries_wtn);
         fflush(stdout);
-        
+
+        TLorentzVector Mu4Momentum,Tau4Momentum, Z4Momentum, Met4Momentum,Ele4Momentum;
+        TLorentzVector LeadTau4Momentum,SubTau4Momentum;
+
         //=========================================================================================================
         //MET Shape systematics
 //        Met=pfMET;
 //        Metphi=pfMETPhi;
-        Met=pfMetNoRecoil;
-        Metphi=pfMetPhiNoRecoil;
-        
-        TLorentzVector Mu4Momentum,Tau4Momentum, Z4Momentum, Met4Momentum;
+        float Met=pfMetNoRecoil;
+        float Metphi=pfMetPhiNoRecoil;
+        Met4Momentum.SetPtEtaPhiM(Met, 0, Metphi, 0);
+                    // Lumi weight
+        float LumiWeight = XSection(sample)*1.0 / HistoTot->GetBinContent(2);
+
         //=========================================================================================================
+        
+        if (channel_tree.find("mutau_tree") != string::npos) {
+        
         // Muon selection
         int idx_lep= lepIndex;
-        
         if (muPt->at(idx_lep) < 30 || fabs(muEta->at(idx_lep)) > 2.4) continue;
         Mu4Momentum.SetPtEtaPhiM(muPt->at(idx_lep),muEta->at(idx_lep),muPhi->at(idx_lep),MuMass);
-                    
-        //=========================================================================================================
         // Tau selection
         int idx_tau= tauIndex;
-        
         if (boostedTauPt->at(idx_tau) <= 30 || fabs(boostedTauEta->at(idx_tau)) >= 2.3 ) continue;
         Tau4Momentum.SetPtEtaPhiM(boostedTauPt->at(idx_tau),boostedTauEta->at(idx_tau),boostedTauPhi->at(idx_tau),boostedTauMass->at(idx_tau));
 
-        //=========================================================================================================
-
-
-        // Event Selection
-        Met4Momentum.SetPtEtaPhiM(Met, 0, Metphi, 0);
-
         TLorentzVector higgs = Tau4Momentum+Mu4Momentum +Met4Momentum;
         TLorentzVector LeadJet= getLeadJet(Mu4Momentum, Tau4Momentum);
-        
         if (higgs.Pt() < 280) continue;
-
-            
-                
-                
-            
-            
-            // Lumi weight
-            LumiWeight = XSection(sample)*1.0 / HistoTot->GetBinContent(2);
-            
-            
-            plotFill("HiggspT",higgs.Pt() ,40,0,2000,LumiWeight);
-            plotFill("LeadJetPt",LeadJet.Pt() ,20,0,1000,LumiWeight);
-
-            higpt->Fill(higgs.Pt(),LumiWeight);
-
+        higpt->Fill(higgs.Pt(),LumiWeight);
         
-        //###############################################################################################
-        //  tree branches
-        //###############################################################################################
-        
-        higgs_pT = higgs.Pt();
-        higgs_m = higgs.M();
-        OS = muCharge->at(idx_lep) * boostedTauCharge->at(idx_tau) < 0;
-        SS =  muCharge->at(idx_lep) * boostedTauCharge->at(idx_tau) > 0;
-        lepPt_=muPt->at(idx_lep);
-        taupt_=boostedTauPt->at(idx_tau);
-        LeadJetPt = LeadJet.Pt();
-        dR_Z_jet=LeadJet.DeltaR(Z4Momentum);
-        m_sv_=m_sv;
-        //  Weights
-        FullWeight = LumiWeight;
-        gen_higgs_pT = GetHiggsPt();
-        
-        
-        // Fill the tree
-        outTr->Fill();
-        
+        }
+    
+    
+    
     } //End of Tree
     
     
     fout->cd();
+    
     map<string, TH1F*>::const_iterator iMap1 = myMap1->begin();
     map<string, TH1F*>::const_iterator jMap1 = myMap1->end();
-    
     for (; iMap1 != jMap1; ++iMap1)
     nplot1(iMap1->first)->Write();
-    outTr->Write();
     
     map<string, TH2F*>::const_iterator iMap2 = myMap2->begin();
     map<string, TH2F*>::const_iterator jMap2 = myMap2->end();
-    
     for (; iMap2 != jMap2; ++iMap2)
     nplot2(iMap2->first)->Write();
+
     higpt->Write();
     fout->Close();
 }
