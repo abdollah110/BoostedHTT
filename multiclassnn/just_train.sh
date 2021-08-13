4f98bd4ec89d1241886313ba3166e7ac93517647
abdollah110 <smohamadi@gmail.com>
2021-08-13 11:31:03 -0500

    script to rin NN

--- multiclassnn/just_train.sh
+++ multiclassnn/just_train.sh
@@ -1,9 +1,9 @@
-version=$1
+input=$1
 year=$2
+version=$3
 
-	echo 'processing ' ${year}
-        python preprocess.py --em-input  /hdfs/store/user/abdollah/PreApproval_rootFiles_${version}/AllCondor_${year}_v11/NN_NOMINAL/ -o Training_${version}_${year} -s 1   
-
-	python train.py --signal _qqH_inc --background TT --background2 ZTT --input datasets/Training_${version}_${year}.h5 --category vbf --year ${year} --model outputModel_${version}_${year}
+echo 'processing ' ${year}
+python preprocess.py -i $input -o Training_${version}_${year} -s 1
+python train.py --signal JJH125 --background QCD --background2 ZTT --input datasets/Training_${version}_${year}.h5  --year ${year} --model outputModel_${version}_${year}
 
 
