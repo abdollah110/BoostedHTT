fc8bf30cae666db291d3af5a04225fadff17df98
abdollah110 <smohamadi@gmail.com>
2021-09-03 09:21:57 -0500

    update xrun

--- Analysis/xrun.py
+++ Analysis/xrun.py
@@ -1,22 +1,30 @@
 import os
+from subprocess import call
 from optparse import OptionParser
 
 
 parser = OptionParser(description='script to make the template and run plot')
-
-parser.add_option('--exe', '-e', action='store',
-                default=False, dest='exe',
-                help='executable'
-                )
-
 parser.add_option('--input', '-i', action='store',
                     default=False, dest='inputFile',
                     help='Location of input file'
                 )
 
+parser.add_option('--prefix', '-p', action='store',
+                    default=False, dest='prefix',
+                    help='prefix'
+                )
+
+parser.add_option('--Observable', '-o', action='store',
+                    default=False, dest='Obs',
+                    help='The observable variable'
+                )
+
+
 
 (options, args) = parser.parse_args()
 InputFile=options.inputFile
+prefix=options.prefix
+Observable=options.Obs
 
 year=0
 if '2016' in InputFile:
@@ -30,46 +38,91 @@ else:
 
 channel=''
 treeName=''
+executable=''
+
 
 if '_em_' in InputFile:
     channel = 'em'
     treeName = 'emu_tree'
+    executable = 'process_trees_em'
+elif '_me_' in InputFile:
+    channel = 'me'
+    treeName = 'mue_tree'
+    executable = 'process_trees_em'
 elif '_mt_' in InputFile:
     channel = 'mt'
     treeName = 'mutau_tree'
+#    executable = 'process_trees_lt'
+    executable = 'process_trees_lt_fr'
+#    executable = 'process_trees_sf_fr'
 elif '_et_' in InputFile:
     channel = 'et'
     treeName = 'etau_tree'
+#    executable = 'process_trees_lt'
+    executable = 'process_trees_lt_fr'
 elif '_tt_' in InputFile:
     channel = 'tt'
     treeName = 'tautau_tree'
+    executable = 'process_trees_tt'
 elif '_mm_' in InputFile:
     channel = 'mm'
     treeName = 'mumu_tree'
+    executable = 'process_trees_mm'
 else:
     print 'which channel ???'
 
 
 
 Variable=[
-
+#
             ['lep1Pt',30,0,300],
             ['lep2Pt',30,0,300],
-            ['leadPt',30,0,300],
-            ['subPt',30,0,300],
             ['tmass',20, 0, 100],
 ##            ['ZMass',20, 0, 200],
-            ['higgs_pT',50, 0, 1000],
+            ['higgs_pT',25, 0, 1000],
             ['higgs_m',20, 0, 400],
             ['Met', 25, 0, 500],
-            ['vis_mass',12, 0, 120],
+            ['vis_mass',15, 0, 150],
             ['dR_lep_lep', 20,0,1],
             ['LeadJetPt', 30,0,1500],
-            ['ht', 20,0,2000],
-            ['st', 20,0,2000],
-            ['m_sv', 20,0,200],
-            ['NN_disc',20,0,1]
+            ['ht', 25,0,2000],
+            ['st', 25,0,2000],
+            ['m_sv', 25,0,250],
+#            ['BoostedTauRawIso', 20,0,1],
+            ['NN_disc',20,0,1],
+#            ['nbjet',5,0,5]
+
 ]
-for var in Variable:
-    print '=====> start making datacard for ', var
-    os.system('./%s -y %s -t %s  -c %s -d %s   --suf %s -v %s -b %f %f %f '%(options.exe, year, treeName, channel, InputFile, var[0],var[0],var[1],var[2],var[3]))
+
+PTrange= [
+        [0,100000,'_bin0'],
+        [0,350,'_bin1'],
+        [350,500,'_bin2'],
+        [500,100000,'_bin3']
+]
+
+Diff=0
+
+if Diff:
+    for pt in PTrange:
+        for var in Variable:
+            if Observable==var[0]:
+                print '\n\n\n\n =====> start making datacard for ', var
+                print 'command is ' , './{} -d {}   --suf {} -v {} -b {} {} {} -c higgs_pT -l {} -h {}  '.format(executable, InputFile, var[0],var[0],var[1],var[2],var[3],pt[0],pt[1])
+                os.system('./{} -d {}   --suf {} -v {} -b {} {} {} -c higgs_pT -l {} -h {}  '.format(executable, InputFile, pt[2],var[0],var[1],var[2],var[3],pt[0],pt[1]))
+                break
+
+else:
+
+    for var in Variable:
+        if Observable==var[0]:
+            print '\n\n\n\n =====> start making datacard for ', var
+            print 'command is ' , './%s -d %s   --suf %s -v %s -b %f %f %f '%(executable, InputFile, var[0],var[0],var[1],var[2],var[3])
+            os.system('./%s -d %s   --suf %s -v %s -b %f %f %f '%(executable, InputFile, prefix,var[0],var[1],var[2],var[3]))
+            break
+
+    for var in Variable:
+        if Observable=='all':
+            print '\n\n\n\n =====> start making datacard for ', var
+            print 'command is ' , './%s -d %s   --suf %s -v %s -b %f %f %f '%(executable, InputFile, var[0],var[0],var[1],var[2],var[3])
+            os.system('./%s -d %s   --suf %s -v %s -b %f %f %f '%(executable, InputFile, prefix,var[0],var[1],var[2],var[3]))
