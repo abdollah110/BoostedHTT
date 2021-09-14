92c07bbc62803868d96c65d1a61d66004c9710cc
abdollah110 <smohamadi@gmail.com>
2021-09-14 17:03:33 -0500

    nominal training for all 3 years

---
+++ multiclassnn/Nominal_Training.sh
@@ -0,0 +1,9 @@
+input=$1
+channel=$2
+
+echo 'processing ' ${channel}
+python preprocess.py -i $input -o NominalTraining_${channel} -s 1
+python train.py --signal JJH125 --background QCD --background2 ZTT --input datasets/NominalTraining_${channel}.h5  --model outputModel_NominalTraining_${channel}
+    
+
+
