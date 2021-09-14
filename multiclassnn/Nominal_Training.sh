3eb40a9e768db52c26860ba2022662019db71392
abdollah110 <smohamadi@gmail.com>
2021-09-14 17:13:46 -0500

    replace QCD with nonDY

--- multiclassnn/Nominal_Training.sh
+++ multiclassnn/Nominal_Training.sh
@@ -3,7 +3,7 @@ channel=$2
 
 echo 'processing ' ${channel}
 python preprocess.py -i $input -o NominalTraining_${channel} -s 1
-python train.py --signal JJH125 --background QCD --background2 ZTT --input datasets/NominalTraining_${channel}.h5  --model outputModel_NominalTraining_${channel}
+python train.py --signal JJH125 --background NonDY --background2 ZTT --input datasets/NominalTraining_${channel}.h5  --model outputModel_NominalTraining_${channel}
     
 
 
