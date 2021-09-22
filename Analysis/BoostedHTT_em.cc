0cfd19b5550343f55c0162ab562b9da8d1b2d886
abdollah110 <smohamadi@gmail.com>
2021-09-22 14:19:06 -0500

    matched boosted id

--- Analysis/BoostedHTT_em.cc
+++ Analysis/BoostedHTT_em.cc
@@ -283,7 +283,20 @@ int main(int argc, char* argv[]) {
                                 
         LepCorrection= MuIdCorrection *  EleIdCorrection;
         
-        
+        bool MuMatchedTauStatus=MatchedBoostedTauId(Mu4Momentum);
+        bool muonIsoStatu = IsoLep1Value < LeptonIsoCut;
+        plotFill("IsoCorrelationMu",muonIsoStatu, MuMatchedTauStatus, 2, 0, 2, 2, 0, 2);
+//        plotFill("cutFlowTable",2 ,15,0,15);
+        if (!MuMatchedTauStatus ) continue;
+
+
+        bool EleMatchedTauStatus=MatchedBoostedTauId(Ele4Momentum);
+        bool eleIsoStatu = IsoLep2Value < LeptonIsoCut;
+        plotFill("IsoCorrelationEle",eleIsoStatu, EleMatchedTauStatus, 2, 0, 2, 2, 0, 2);
+//        plotFill("cutFlowTable",2 ,15,0,15);
+        if (!EleMatchedTauStatus ) continue;
+
+
         //=========================================================================================================
         // Event Selection
         Met4Momentum.SetPtEtaPhiM(Met, 0, Metphi, 0);
