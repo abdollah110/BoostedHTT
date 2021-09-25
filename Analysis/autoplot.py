c526f440a85a3e9422d1e662da8546b1f8ed64f3
abdollah110 <smohamadi@gmail.com>
2021-09-25 15:14:14 -0500

    fix the naming issue

--- Analysis/autoplot.py
+++ Analysis/autoplot.py
@@ -2,32 +2,33 @@ import plotter
 
 
 plots = [
-#    ('NN_disc', 'D_{NN}', 3),
+    ('NN_disc', 'D_{NN}', 3),
     ('m_sv', 'm_{SVFit}', 3.0),
+    ('Met', 'Missing E_{T}', 3.0),
+    ('ht', 'ht', 3.0),    
     ('higgs_pT', 'Higgs p_{T}', 3.0),
     ('higgs_m', 'Higgs mass', 3.0),
     ('tmass', 'm_{T}', 3.0),
-    ('Met', 'Missing E_{T}', 3.0),
     ('vis_mass', 'm_{Visible}', 3.0),
     ('lep1Pt', '1st lepton pT', 3.0),
     ('lep2Pt', '2nd lepton pT', 3.0),
     ('dR_lep_lep', 'dR_{#tau #tau}', 3.0),
     ('LeadJetPt', 'LeadJetPt', 3.0),
-    ('ht', 'ht', 3.0),
     ('st', 'st', 3.0),
-###    ('ZMass', 'ZMass', 3.0),
-    ('BoostedTauRawIso', 'BoostedTauRawIso', 5.0),
-#    ('nbjet', 'nbjet', 3.0),
-#
+####    ('ZMass', 'ZMass', 3.0),
+#    ('BoostedTauRawIso', 'BoostedTauRawIso', 5.0),
+##    ('nbjet', 'nbjet', 3.0),
+##
 ]
 
 categories = ['_0jet']
 
 class PlotArgs():
-    def __init__(self, input, year, category, variable, label, prefix, scale):
+    def __init__(self, input, year, category,channelName, variable, label, prefix, scale):
         self.input = input
         self.year = year
         self.category = category
+        self.channelName = channelName
         self.variable = variable
         self.label = label
         self.prefix = prefix
@@ -41,6 +42,7 @@ def main(args):
 
             InputFile=args.input
             channel=''
+            channelName=''
     #        treeName=''
             year=0
             
@@ -54,20 +56,30 @@ def main(args):
                 print 'which year ???'
 
 
-            if 'em2' in InputFile:
+            if 'em2' in InputFile or '_em_' in InputFile:
                 channel = 'em'
+                channelName='em'
     #            treeName = 'emu_tree'
-            elif 'mt2' in InputFile:
+            elif 'me2' in InputFile or    '_me_' in InputFile:
+#                channel = 'me'
+                channel = 'em'
+                channelName='me'
+                treeName = 'mue_tree'
+            elif 'mt2' in InputFile or '_mt_' in InputFile:
                 channel = 'mt'
+                channelName='mt'
                 treeName = 'mutau_tree'
-            elif 'et2' in InputFile:
+            elif 'et2' in InputFile  or  '_et_' in InputFile:
                 channel = 'et'
+                channelName='et'
     #            treeName = 'etau_tree'
-            elif 'tt2' in InputFile:
+            elif 'tt2' in InputFile  or  '_tt_' in InputFile:
                 channel = 'tt'
+                channelName='tt'
     #            treeName = 'tautau_tree'
             elif 'mm2' in InputFile:
                 channel = 'mm'
+                channelName='mm'
     #            treeName = 'mumu_tree'
             else:
                 print 'which channel ???'
@@ -78,6 +90,7 @@ def main(args):
                 args.input,
                 year,
                 channel + category,
+                channelName,
                 plot[0],
                 plot[1],
                 args.prefix,
--- multiclassnn/Nominal_Training.sh
+++ multiclassnn/Nominal_Training.sh
@@ -2,8 +2,8 @@ input=$1
 channel=$2
 
 echo 'processing ' ${channel}
-python preprocess.py -i $input -o NominalTraining_${channel} -s 1
-python train.py --signal JJH125 --background NonDY --background2 ZTT --input datasets/NominalTraining_${channel}.h5  --model outputModel_NominalTraining_${channel}
+python preprocess.py -i $input -o NominalPreprocess_${channel} -s 1
+python train.py --signal JJH125 --background NonDY --background2 ZTT --input datasets/NominalPreprocess_${channel}.h5  --model outputModel_NominalTraining_${channel}
     
 
 
