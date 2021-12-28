82da2301074995c9e6a349287a5e9f396dc307a6
abdollah110 <smohamadi@gmail.com>
2021-12-27 18:22:57 -0600

    preProoval comments

---
+++ Limit/BoostedTau.cpp
@@ -0,0 +1,392 @@
+#include <string>
+#include <map>
+#include <set>
+#include <iostream>
+#include <vector>
+#include <utility>
+#include <cstdlib>
+#include "boost/algorithm/string/predicate.hpp"
+#include "boost/program_options.hpp"
+#include "boost/lexical_cast.hpp"
+#include "boost/regex.hpp"
+#include "boost/filesystem.hpp"
+#include "CombineHarvester/CombineTools/interface/CombineHarvester.h"
+#include "CombineHarvester/CombineTools/interface/Utilities.h"
+#include "CombineHarvester/CombineTools/interface/HttSystematics.h"
+#include "CombineHarvester/CombineTools/interface/CardWriter.h"
+#include "CombineHarvester/CombineTools/interface/CopyTools.h"
+#include "CombineHarvester/CombineTools/interface/BinByBin.h"
+#include "CombineHarvester/CombineTools/interface/Systematics.h"
+
+namespace po = boost::program_options;
+using boost::starts_with;
+using namespace std;
+
+int main(int argc, char** argv) {
+    
+    string postfix="";
+    string prefix="";
+    string year = "2016";
+    string inputFile="";
+    string WP="WP";
+    string Var="NN";
+    po::variables_map vm;
+    po::options_description config("configuration");
+    config.add_options()
+    //    ("mass,m", po::value<string>(&mass)->default_value(mass))
+    //      ("input_folder_em", po::value<string>(&input_folder_em)->default_value("USCMS"))
+    //      ("input_folder_et", po::value<string>(&input_folder_et)->default_value("USCMS"))
+    //      ("input_folder_mt", po::value<string>(&input_folder_mt)->default_value("USCMS"))
+    //      ("input_folder_tt", po::value<string>(&input_folder_tt)->default_value("USCMS"))
+    //      ("input_folder_mm", po::value<string>(&input_folder_mm)->default_value("USCMS"))
+    //      ("input_folder_ttbar", po::value<string>(&input_folder_ttbar)->default_value("USCMS"))
+    ("prefix", po::value<string>(&prefix)->default_value(""))
+    ("postfix", po::value<string>(&postfix)->default_value(""))
+    ("Var", po::value<string>(&Var)->default_value(""))
+    //      ("vbfcateStr_tt", po::value<string>(&vbfcateStr_tt)->default_value("tt_vbf_ggHMELA_bin"))
+    //      ("vbfcateStr_mt", po::value<string>(&vbfcateStr_mt)->default_value("mt_vbf_ggHMELA_bin"))
+    //      ("auto_rebin", po::value<bool>(&auto_rebin)->default_value(false))
+    //      ("real_data", po::value<bool>(&real_data)->default_value(false))
+    //      ("manual_rebin", po::value<bool>(&manual_rebin)->default_value(false))
+    //      ("output_folder", po::value<string>(&output_folder)->default_value("sm_run2"))
+    //      ("SM125,h", po::value<string>(&SM125)->default_value(SM125))
+    //      ("control_region", po::value<int>(&control_region)->default_value(0))
+    ("year", po::value<string>(&year)->default_value("2016"))
+    ("WP", po::value<string>(&WP)->default_value(""))
+    ("inputFile", po::value<string>(&inputFile)->default_value(""));
+    //      ("mm_fit", po::value<bool>(&mm_fit)->default_value(true))
+    //      ("ttbar_fit", po::value<bool>(&ttbar_fit)->default_value(true))
+    //      ("jetfakes", po::value<bool>(&do_jetfakes)->default_value(false))
+    //      ("embedded", po::value<bool>(&do_embedded)->default_value(false))
+    //      ("shapeSyst", po::value<bool>(&do_shapeSyst)->default_value(false))
+    //      ("sync", po::value<bool>(&do_sync)->default_value(false))
+    //      ("useSingleVBFdir", po::value<bool>(&useSingleVBFdir)->default_value(false))
+    //      ("chn", po::value<string>(&do_chn)->default_value("tt"))
+    //      ("par", po::value<string>(&par)->default_value("fa3"))
+    //      ("is2017", po::value<bool>(&is_2017)->default_value(false))
+    //      ("use_ggHint", po::value<bool>(&use_ggHint)->default_value(false))
+    //      ("check_neg_bins", po::value<bool>(&check_neg_bins)->default_value(false))
+    //      ("poisson_bbb", po::value<bool>(&poisson_bbb)->default_value(false))
+    //      ("w_weighting", po::value<bool>(&do_w_weighting)->default_value(false));
+    po::store(po::command_line_parser(argc, argv).options(config).run(), vm);
+    po::notify(vm);
+    
+    
+    
+    
+    
+    
+    
+    
+    //! [part1]
+    // First define the location of the "auxiliaries" directory where we can
+    // source the input files containing the datacard shapes
+    //    string aux_shapes = string(getenv("CMSSW_BASE")) + "/src/auxiliaries/shapes/";
+    string aux_shapes = string(getenv("CMSSW_BASE")) + "/src/CombineHarvester/CombineTools/bin/aux/";
+    
+    // Create an empty CombineHarvester instance that will hold all of the
+    // datacard configuration and histograms etc.
+    ch::CombineHarvester cb;
+    
+    typedef vector<pair<int, string>> Categories;
+    typedef vector<string> VString;
+    // Uncomment this next line to see a *lot* of debug information
+    // cb.SetVerbosity(3);
+    
+    // Here we will just define two categories for an 8TeV analysis. Each entry in
+    // the vector below specifies a bin name and corresponding bin_id.
+    
+    //    VString chns = { "mt","et","em","me","tt"};
+    VString chns = { "mt","et","em","tt"};
+    
+    map<string, string> input_folders = {
+        {"mt", "."},
+        {"et", "."},
+        {"em", "."},
+        //        {"me", "."},
+        {"tt", "."}
+    };
+    
+    map<string, VString> bkg_procs;
+    bkg_procs["mt"] = {"QCD", "TT","VV","ZTT"};
+    bkg_procs["et"] = {"QCD", "TT","VV","ZTT"};
+    bkg_procs["em"] = {"QCD","W", "TT","VV","ZTT"};
+    //    bkg_procs["me"] = {"W", "TT","VV","ZTT"};
+    bkg_procs["tt"] = {"QCD", "TT","VV","ZTT"};
+    
+    VString sig_procs = {"ggH","XH"};
+    //    VString sig_procs = {"ggH"};
+    
+    map<string, Categories> cats;
+    //cats["et_13TeV"] = {
+    //      {1, "EleTau_DiJet"}};
+    
+    
+    cats["mt_13TeV"] = {
+        {1, "mt_0jet"},
+        //        {2, "fail"}
+    };
+    cats["et_13TeV"] = {
+        {1, "et_0jet"},
+        //        {2, "fail"}
+    };
+    cats["em_13TeV"] = {
+        {1, "em_0jet"},
+        //        {2, "fail"}
+    };
+    //    cats["me_13TeV"] = {
+    //        {1, "me_0jet"},
+    ////        {2, "fail"}
+    //    };
+    cats["tt_13TeV"] = {
+        {1, "tt_0jet"},
+        //        {2, "fail"}
+    };
+    
+    
+    // ch::Categories is just a typedef of vector<pair<int, string>>
+    //! [part1]
+    
+    
+    //! [part2]
+    //    vector<string> masses = ch::MassesFromRange("800-1500:100");
+    //    vector<string> masses = ch::MassesFromRange("14-15:1");
+    //vector<string> masses = ch::MassesFromRange("14-15:1");
+    vector<string> masses = {"125"};
+    // Or equivalently, specify the mass points explicitly:
+    //! [part2]
+    
+    
+    //! [part4]
+    for (auto chn : chns) {
+        cb.AddObservations(
+                           {"*"}, {"H"}, {"13TeV"}, {chn}, cats[chn+"_13TeV"]);
+        cb.AddProcesses(
+                        {"*"}, {"H"}, {"13TeV"}, {chn}, bkg_procs[chn], cats[chn+"_13TeV"], false);
+        cb.AddProcesses(
+                        masses, {"H"}, {"13TeV"}, {chn}, sig_procs, cats[chn+"_13TeV"], true);
+    }
+    
+    
+    
+    
+    cout << ">> Extracting histograms from input root files...\n";
+    for (string era : {"13TeV"}) {
+        for (string chn : chns) {
+            
+            string file = aux_shapes + input_folders[chn] + "/"+prefix+"_"+year+"_"+Var+".root";
+            cb.cp().channel({chn}).era({era}).backgrounds().ExtractShapes(
+                                                                          file, "$BIN/$PROCESS", "$BIN/$PROCESS_$SYSTEMATIC");
+            cb.cp().channel({chn}).era({era}).signals().ExtractShapes(
+                                                                      file, "$BIN/$PROCESS$MASS", "$BIN/$PROCESS$MASS_$SYSTEMATIC");
+        }
+    }
+    
+    
+    //Some of the code for this is in a nested namespace, so
+    // we'll make some using declarations first to simplify things a bit.
+    using ch::syst::SystMap;
+    using ch::syst::era;
+    using ch::syst::bin_id;
+    using ch::syst::process;
+    using ch::JoinStr;
+    
+    
+    //####################################################################################
+    // Norm systematics
+    //####################################################################################
+    
+    cb.cp().process(ch::JoinStr({sig_procs, {"W", "TT","VV","ZTT"}}))
+    .AddSyst(cb, "CMS_lumi_$ERA", "lnN", SystMap<era>::init({"13TeV"}, 1.025));
+    
+    //        tau
+    cb.cp().process(ch::JoinStr({sig_procs, {"TT","VV","ZTT"}})).channel({"tt"})
+    .AddSyst(cb, "CMS_trg_t$ERA", "lnN", SystMap<era>::init({"13TeV"}, 1.10));
+    
+    cb.cp().process(ch::JoinStr({sig_procs, {"TT","VV","ZTT"}})).channel({"et","mt","tt"})
+    .AddSyst(cb, "CMS_eff_t$ERA", "lnN", SystMap<era>::init({"13TeV"}, 1.10));
+    
+    //         electron
+    cb.cp().process(ch::JoinStr({sig_procs, {"W","TT","VV","ZTT"}})).channel({"et","em"})
+    .AddSyst(cb, "CMS_eff_e$ERA", "lnN", SystMap<era>::init({"13TeV"}, 1.02));
+    
+    cb.cp().process(ch::JoinStr({sig_procs, {"W","TT","VV","ZTT"}})).channel({"et","em"})
+    .AddSyst(cb, "CMS_scale_e$ERA", "lnN", SystMap<era>::init({"13TeV"}, 1.02));
+    
+    cb.cp().process(ch::JoinStr({sig_procs, {"W","TT","VV","ZTT"}})).channel({"et","em"})
+    .AddSyst(cb, "CMS_trg_e$ERA", "lnN", SystMap<era>::init({"13TeV"}, 1.02));
+    
+    //      muon
+    cb.cp().process(ch::JoinStr({sig_procs, {"W", "TT","VV","ZTT"}})).channel({"mt","em"})
+    .AddSyst(cb, "CMS_eff_m$ERA", "lnN", SystMap<era>::init({"13TeV"}, 1.02));
+    
+    cb.cp().process(ch::JoinStr({sig_procs, {"W", "TT","VV","ZTT"}})).channel({"mt","em"})
+    .AddSyst(cb, "CMS_scale_m$ERA", "lnN", SystMap<era>::init({"13TeV"}, 1.02));
+    
+    cb.cp().process(ch::JoinStr({sig_procs, {"W", "TT","VV","ZTT"}})).channel({"mt","em"})
+    .AddSyst(cb, "CMS_trg_m$ERA", "lnN", SystMap<era>::init({"13TeV"}, 1.02));
+    
+    //      JER
+    
+    cb.cp().process(ch::JoinStr({sig_procs, {"W", "TT","VV","ZTT"}}))
+    .AddSyst(cb, "CMS_JER$ERA", "lnN", SystMap<era>::init({"13TeV"}, 1.01));
+    
+    cb.cp().process({"ZTT"})
+    .AddSyst(cb, "CMS_htt_ZTTNorm", "lnN", SystMap<>::init(1.03));
+    
+    cb.cp().process({"TT"})
+    .AddSyst(cb, "CMS_htt_TTNorm", "lnN", SystMap<>::init(1.05));
+    
+    cb.cp().process({"VV"})
+    .AddSyst(cb, "CMS_htt_VVNorm", "lnN", SystMap<>::init(1.05));
+        
+    cb.cp().process({"W"})
+    .AddSyst(cb, "CMS_htt_WNorm", "lnN", SystMap<>::init(1.10));
+    
+    cb.cp().process({"QCD"})
+    .AddSyst(cb, "CMS_htt_QCDNorm", "lnN", SystMap<>::init(1.20));
+    
+    //####################################################################################
+    // Shape systematics
+    //####################################################################################
+        
+    cb.cp().process(ch::JoinStr({sig_procs, {"W", "TT","VV","ZTT"}}))
+    .AddSyst(cb, "JEnTot", "shape", SystMap<>::init(1.00));
+//
+////    cb.cp().process(ch::JoinStr({sig_procs, {"W", "TT","VV","ZTT"}}))
+////    .AddSyst(cb, "MissingEn_JES", "shape", SystMap<>::init(1.00));
+//
+    cb.cp().process(ch::JoinStr({sig_procs, {"W", "TT","VV","ZTT"}}))
+    .AddSyst(cb, "MissingEn_UES", "shape", SystMap<>::init(1.00));
+
+    cb.cp().process(ch::JoinStr({sig_procs, {"W", "TT","VV","ZTT"}}))
+    .AddSyst(cb, "TES", "shape", SystMap<>::init(1.00));
+
+    cb.cp().process({"QCD"})
+    .AddSyst(cb, "shape_", "shape", SystMap<>::init(1.00));
+
+
+    cb.cp().process(ch::JoinStr({sig_procs, {"TT","VV","ZTT"}}))
+    .AddSyst(cb, "prefire", "shape", SystMap<>::init(1.00));
+
+    cb.cp().process({"TT"})
+    .AddSyst(cb, "ttbarShape_", "shape", SystMap<>::init(1.00));
+
+    cb.cp().process({"ZTT"})
+    .AddSyst(cb, "Z_masspt_", "shape", SystMap<>::init(1.00));
+    
+    //####################################################################################
+    // Theorethical systematics
+    //####################################################################################
+
+    cb.cp().process({"ggH"})
+    .AddSyst(cb, "THU_ggH_Mu_", "shape", SystMap<>::init(1.00));
+
+    cb.cp().process({"ggH"})
+    .AddSyst(cb, "THU_ggH_Res_", "shape", SystMap<>::init(1.00));
+
+    cb.cp().process({"ggH"})
+    .AddSyst(cb, "THU_ggH_Mig01_", "shape", SystMap<>::init(1.00));
+
+    cb.cp().process({"ggH"})
+    .AddSyst(cb, "THU_ggH_Mig12_", "shape", SystMap<>::init(1.00));
+
+    cb.cp().process({"ggH"})
+    .AddSyst(cb, "THU_ggH_VBF2j_", "shape", SystMap<>::init(1.00));
+
+    cb.cp().process({"ggH"})
+    .AddSyst(cb, "THU_ggH_VBF3j_", "shape", SystMap<>::init(1.00));
+
+    cb.cp().process({"ggH"})
+    .AddSyst(cb, "THU_ggH_PT60_", "shape", SystMap<>::init(1.00));
+
+    cb.cp().process({"ggH"})
+    .AddSyst(cb, "THU_ggH_PT120_", "shape", SystMap<>::init(1.00));
+
+    cb.cp().process({"ggH"})
+    .AddSyst(cb, "THU_ggH_qmtop_", "shape", SystMap<>::init(1.00));
+    
+    
+//    cout << ">> Adding systematic uncertainties...\n";
+    // ch::AddSystematics_et_mt(cb);
+    
+    //! [part6]
+    
+    //! [part7]
+    
+    cb.cp().backgrounds().ExtractShapes(
+                                        aux_shapes + "/"+prefix+"_"+year+"_"+Var+".root",
+                                        "$BIN/$PROCESS",
+                                        "$BIN/$PROCESS_$SYSTEMATIC");
+    cb.cp().signals().ExtractShapes(
+                                    aux_shapes + "/"+prefix+"_"+year+"_"+Var+".root",
+                                    "$BIN/$PROCESS$MASS",
+                                    "$BIN/$PROCESS$MASS_$SYSTEMATIC");
+    
+    
+    
+    
+    //! [part7]
+    
+    //! [part8]
+    //    auto bbb = ch::BinByBinFactory()
+    //    .SetAddThreshold(0.1)
+    //    .SetFixNorm(true);
+    
+    //    bbb.AddBinByBin(cb.cp().backgrounds(), cb);
+    
+    // This function modifies every entry to have a standardised bin name of
+    // the form: {analysis}_{channel}_{bin_id}_{era}
+    // which is commonly used in the htt analyses
+    ch::SetStandardBinNames(cb);
+    
+    
+    
+    //! [part8]
+    
+    //! [part9]
+    // First we generate a set of bin names:
+    set<string> bins = cb.bin_set();
+    // This method will produce a set of unique bin names by considering all
+    // Observation, Process and Systematic entries in the CombineHarvester
+    // instance.
+    
+    // We create the output root file that will contain all the shapes.
+    //TFile output("RHW_mt.inputs.root", "RECREATE");
+    
+    // Finally we iterate through each bin,mass combination and write a
+    
+    
+    auto pre_drop = cb.syst_name_set();
+    //cb.syst_name(droplist, false);
+    auto post_drop = cb.syst_name_set();
+    cout << ">> Systematics dropped: " << pre_drop.size() - post_drop.size()
+    << "\n";
+    
+    
+    string folder = postfix+"/"+year;
+    boost::filesystem::create_directories(folder);
+    boost::filesystem::create_directories(folder + "/common");
+    for (auto m : masses) {
+        boost::filesystem::create_directories(folder + "/" + m);
+    }
+    
+    for (string chn : chns) {
+        TFile output((folder + "/common/"+chn+"_"+year+"_"+Var+".root").c_str(),
+                     "RECREATE");
+        auto bins = cb.cp().channel({chn}).bin_set();
+        for (auto b : bins) {
+            for (auto m : masses) {
+                cout << ">> Writing datacard for bin: " << b << " and mass: " << m
+                << "\r" << flush;
+                cb.cp().channel({chn}).bin({b}).mass({m, "*"}).WriteDatacard(
+                                                                             folder + "/" + m + "/" + b + ".txt", output);
+            }
+        }
+        output.Close();
+    }
+    
+    
+    //! [part9]
+    
+}
---
+++ Skim/makeSubmissionPackage_Diff.py
@@ -0,0 +1,79 @@
+# abdollah.mohammadi@cern.ch
+###### This is the script for creating scripts for submitting jobs on LPC condor for skimming of ggNtuples
+# python makeSubmissionPackage.py -x SkimerBoost_mt -i InputSample.txt -n muTau_v1 -o /store/user/abdollah/NewSkimBoost/mt/v1
+######
+import os
+import sys
+import argparse
+
+parser = argparse.ArgumentParser(description='create sumission files from the existing templates')
+parser.add_argument('-x','--executable',action='store',default='SkimerBoost_mt',dest='exe', help='the name of the executable')
+parser.add_argument('-i','--input',action='store',default='InputSample.txt',dest='InputSample', help='the name of the INpitSamples')
+parser.add_argument('-n','--name',action='store',default='_tt_v1',dest='Name', help='suffix to the script names')
+parser.add_argument('-o','--output',action='store',default='/store/user/abdollah/NewSkimBoost/mt/v2',dest='OutPut', help='Location of the output file')
+args = parser.parse_args()
+
+
+## template for Submit_run
+Out_run=open('Submit_run_%s.sh'%args.Name,'w')
+run_file=open('Template_Submit_run.sh','r')
+for line in run_file.readlines():
+
+    line=line.replace('abdollah',os.environ['LOGNAME'])
+    line=line.replace('SkimerBoost_mt',args.exe)
+    line=line.replace('TarFile','Files_%s'%args.Name)
+    line=line.replace('OUTPUTFILE_LOCATION',args.OutPut)
+    line=line.replace('InputSamples.txt',args.InputSample)
+
+
+    Out_run.write(line)
+Out_run.close()
+
+
+## Count number of files
+input_File=open(args.InputSample,'r')
+count=0
+for line in input_File.readlines():
+    if len(line.strip()) == 0 : continue
+    count+=1
+print 'number of file is %d'%count
+
+
+## template for Submit_batch
+Out_batch=open('Submit_batch_%s.jdl'%args.Name,'w')
+run_file=open('Template_Submit_batch.jdl','r')
+for line in run_file.readlines():
+    line=line.replace('abdollah',os.environ['LOGNAME'])
+    line=line.replace('Submit_run.sh','Submit_run_%s.sh'%args.Name)
+    line=line.replace('THE_RUN_PATH', os.getcwd())
+    if 'Queue' in line:
+        line='Queue '+ str(count*10)
+    Out_batch.write(line)
+Out_batch.close()
+
+
+## copy necessary files in the /eos are
+logName=os.environ['LOGNAME']
+os.system('rm -r Files_%s'%args.Name)
+os.system('mkdir Files_%s'%args.Name)
+os.system('mv Submit_run_%s.sh Files_%s/.'%(args.Name,args.Name))
+os.system('mv Submit_batch_%s.jdl Files_%s/.'%(args.Name,args.Name))
+os.system('cp %s Files_%s/.'%(args.exe,args.Name))
+os.system('cp %s Files_%s/.'%(args.InputSample,args.Name))
+os.system('tar -zcp -f Files_%s.tar.gz  Files_%s'%(args.Name,args.Name))
+
+os.system('xrdcp -f Files_%s.tar.gz root://cmseos.fnal.gov://store/user/${LOGNAME}/.'%args.Name)
+
+os.system('mv Files_%s.tar.gz Files_%s/.'%(args.Name,args.Name))
+    
+## Print the condos submit command
+print 'total number of jobs to be submitted will be %d'%(count*10)
+print 'Just run the following command:'
+print '................................'
+#print 'condor_submit Submit_batch_%s.jdl'%args.Name
+print 'cd Files_%s/; condor_submit Submit_batch_%s.jdl; cd -'%(args.Name,args.Name)
+print '................................'
+
+
+
+
