649880fe28df929d9d3646110b499e3c964dc266
abdollah110 <smohamadi@gmail.com>
2022-04-25 15:20:10 -0500

    ReNamingDC

--- Limit/ReNamingDC.py
+++ Limit/ReNamingDC.py
@@ -109,7 +109,8 @@ for k1 in dirList: # loop over categories
         h3=h2.Clone()
         histo_name=h2.GetName()
         print  h3.integral(), h3.GetName()
-        if h3.integral()==0 : print '=========>>>>   zero integral     <<<<<========== ', h3.GetName()
+        
+        if h3.integral() != h3.replace('Up','Down').integral() : print '=========>>>>   zero integral     <<<<<========== ', h3.GetName()
         if (h2.GetName()==name_last):
             continue
         name_last=histo_name
