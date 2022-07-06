c184fa62885accf5d26c8fe1728f3d2a13e7b323
abdollah110 <smohamadi@gmail.com>
2022-07-06 12:20:47 -0500

    change code for qcd validation relax m_sv and H_ptgit

--- Analysis/BoostedHTT_mt.cc
+++ Analysis/BoostedHTT_mt.cc
@@ -373,10 +373,10 @@ int main(int argc, char* argv[]) {
         plotFill("cutFlowTable",6 ,15,0,15);
         
         tmass = TMass_F(Mu4Momentum.Pt(), Mu4Momentum.Px(), Mu4Momentum.Py(),  Met,  Metphi);
-        if (tmass > 80) continue;
+//        if (tmass > 80) continue; FIX ME for Validation
         plotFill("cutFlowTable",7 ,15,0,15);
         
-        if (m_sv < 50) continue;
+//        if (m_sv < 50) continue; FIX ME for Validation
         plotFill("cutFlowTable",8 ,15,0,15);
         
         // BJet veto
@@ -398,7 +398,7 @@ int main(int argc, char* argv[]) {
         plotFill("cutFlowTable",11 ,15,0,15);
         
         //        if (higgs.Pt() < 250) continue;
-        if (higgs.Pt() < 250) continue;
+//        if (higgs.Pt() < 250) continue; FIX ME for Validation
         plotFill("cutFlowTable",12 ,15,0,15);
         
         //=========================================================================================================
