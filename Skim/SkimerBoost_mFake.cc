5d30e7ff7d0865941dc47e921b33da3ab67b9154
abdollah110 <smohamadi@gmail.com>
2022-12-28 12:12:21 -0600

    add oldDM instead of new and add muon rejection

--- Skim/SkimerBoost_mFake.cc
+++ Skim/SkimerBoost_mFake.cc
@@ -69,9 +69,11 @@ void SkimerBoost::Loop(TString OutputFile)
             
             for (int ibtau = 0; ibtau < nBoostedTau; ++ibtau){
                 if (boostedTauPt->at(ibtau) < tauPt_cut || fabs(boostedTauEta->at(ibtau)) > 2.3 ) continue;
-//                if (boostedTaupfTausDiscriminationByDecayModeFinding->at(ibtau) < 0.5 ) continue;
-                if (boostedTaupfTausDiscriminationByDecayModeFindingNewDMs->at(ibtau) < 0.5 ) continue;
+                if (boostedTaupfTausDiscriminationByDecayModeFinding->at(ibtau) < 0.5 ) continue;
+//                if (boostedTaupfTausDiscriminationByDecayModeFindingNewDMs->at(ibtau) < 0.5 ) continue;
                 if (boostedTauByIsolationMVArun2v1DBnewDMwLTrawNew->at(ibtau) < -0.5) continue;
+                if (boostedTauByLooseMuonRejection3->at(ibtau) < 0.5 ) continue;
+                
                 LeadTau4Momentum.SetPtEtaPhiM(boostedTauPt->at(ibtau),boostedTauEta->at(ibtau),boostedTauPhi->at(ibtau),boostedTauMass->at(ibtau));
                 
 //                if (LeadTau4Momentum.DeltaR(LeadMu4Momentum) < 1.5 ) continue;
