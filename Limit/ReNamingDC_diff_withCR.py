8c1ea5271668ed181ffbc761e20897d58950e7ef
abdollah110 <smohamadi@gmail.com>
2023-03-22 16:22:41 -0500

    make the Rename working for both hpt and jpt

--- Limit/ReNamingDC_diff_withCR.py
+++ Limit/ReNamingDC_diff_withCR.py
@@ -122,10 +122,10 @@ for k1 in dirList: # loop over categories
 #    ofile.mkdir(nom.replace('_0jet',''))
     Updatednom=nom.replace('_0jet','')
     
-    if '_bin1' in Updatednom: Updatednom='Htt_{}_0_350_cat{}{}{}'.format(Diff,year,channel,type)
-    if '_bin2' in Updatednom: Updatednom='Htt_{}_350_450_cat{}{}{}'.format(Diff,year,channel,type)
-    if '_bin3' in Updatednom: Updatednom='Htt_{}_450_600_cat{}{}{}'.format(Diff,year,channel,type)
-    if '_bin4' in Updatednom: Updatednom='Htt_{}_GT600_cat{}{}{}'.format(Diff,year,channel,type)
+    if '_bin1' in Updatednom: Updatednom='Htt_{}_0_350_cat{}{}'.format(Diff,year,channel,type)
+    if '_bin2' in Updatednom: Updatednom='Htt_{}_350_450_cat{}{}'.format(Diff,year,channel,type)
+    if '_bin3' in Updatednom: Updatednom='Htt_{}_450_600_cat{}{}'.format(Diff,year,channel,type)
+    if '_bin4' in Updatednom: Updatednom='Htt_{}_GT600_cat{}{}'.format(Diff,year,channel,type)
 
     ofile.mkdir(Updatednom)
     h1.cd()
