110304c2a0414690169f8c24f60a223c90d56843
abdollah110 <smohamadi@gmail.com>
2023-03-22 14:58:50 -0500

    make the Rename working for both hpt and jpt

--- Limit/ReNamingDC_diff_withCR.py
+++ Limit/ReNamingDC_diff_withCR.py
@@ -106,14 +106,22 @@ for k1 in dirList: # loop over categories
 
     print 'year is {} and channel is {}'.format(year,channel)
 
+
+
+    if 'qcd' in nom:
+        type = '_qcd'
+    elif 'ztt' in nom:
+        type = '_ztt'
+    elif 'signal' in nom:
+        type = '_signal'
+
+
     categ=nom.replace(channel+'_','').replace('nal','')
     print 'categ is', categ
 
-
 #    ofile.mkdir(nom.replace('_0jet',''))
     Updatednom=nom.replace('_0jet','')
-    UpdatednomOrg=Updatednom
-    BinName=''
+    
     if '_bin1' in Updatednom: Updatednom='Htt_{}_0_350_cat{}{}{}'.format(Diff,year,channel,type)
     if '_bin2' in Updatednom: Updatednom='Htt_{}_350_450_cat{}{}{}'.format(Diff,year,channel,type)
     if '_bin3' in Updatednom: Updatednom='Htt_{}_450_600_cat{}{}{}'.format(Diff,year,channel,type)
@@ -244,53 +252,6 @@ for k1 in dirList: # loop over categories
         histo_name=histo_name.replace('TESUp_1prong','CMS_scale_t_1prongUp')
         histo_name=histo_name.replace('TESUp_3prong','CMS_scale_t_3prongUp')
 
-#        if channel=='et' :
-#            histo_name=histo_name.replace('CMS_scale_t_1prong1pizero','CMS_scale_t_1prong1pizero_et')
-#            histo_name=histo_name.replace('CMS_scale_t_1prongU','CMS_scale_t_1prong_etU')
-#            histo_name=histo_name.replace('CMS_scale_t_1prongD','CMS_scale_t_1prong_etD')
-#            histo_name=histo_name.replace('CMS_scale_t_3prong','CMS_scale_t_3prong_et')
-#        if channel=='mt' :
-#            histo_name=histo_name.replace('CMS_scale_t_1prong1pizero','CMS_scale_t_1prong1pizero_mt')
-#            histo_name=histo_name.replace('CMS_scale_t_1prongU','CMS_scale_t_1prong_mtU')
-#            histo_name=histo_name.replace('CMS_scale_t_1prongD','CMS_scale_t_1prong_mtD')
-#            histo_name=histo_name.replace('CMS_scale_t_3prong','CMS_scale_t_3prong_mt')
-#        if channel=='tt' :
-#            histo_name=histo_name.replace('CMS_scale_t_1prong1pizero','CMS_scale_t_1prong1pizero_tt')
-#            histo_name=histo_name.replace('CMS_scale_t_1prongU','CMS_scale_t_1prong_ttU')
-#            histo_name=histo_name.replace('CMS_scale_t_1prongD','CMS_scale_t_1prong_ttD')
-#            histo_name=histo_name.replace('CMS_scale_t_3prong','CMS_scale_t_3prong_tt')
-
-
-#        if channel=='et' :
-#            histo_name=histo_name.replace('CMS_scale_t_1prong1pizero','CMS_scale_t_1prong1pizero_et_cat{}'.format(categ))
-#            histo_name=histo_name.replace('CMS_scale_t_1prongU','CMS_scale_t_1prong_et_cat{}U'.format(categ))
-#            histo_name=histo_name.replace('CMS_scale_t_1prongD','CMS_scale_t_1prong_et_cat{}D'.format(categ))
-#            histo_name=histo_name.replace('CMS_scale_t_3prong','CMS_scale_t_3prong_et_cat{}'.format(categ))
-#        if channel=='mt' :
-#            histo_name=histo_name.replace('CMS_scale_t_1prong1pizero','CMS_scale_t_1prong1pizero_mt_cat{}'.format(categ))
-#            histo_name=histo_name.replace('CMS_scale_t_1prongU','CMS_scale_t_1prong_mt_cat{}U'.format(categ))
-#            histo_name=histo_name.replace('CMS_scale_t_1prongD','CMS_scale_t_1prong_mt_cat{}D'.format(categ))
-#            histo_name=histo_name.replace('CMS_scale_t_3prong','CMS_scale_t_3prong_mt_cat{}'.format(categ))
-#        if channel=='tt' :
-#            histo_name=histo_name.replace('CMS_scale_t_1prong1pizero','CMS_scale_t_1prong1pizero_tt_cat{}'.format(categ))
-#            histo_name=histo_name.replace('CMS_scale_t_1prongU','CMS_scale_t_1prong_tt_cat{}U'.format(categ))
-#            histo_name=histo_name.replace('CMS_scale_t_1prongD','CMS_scale_t_1prong_tt_cat{}D'.format(categ))
-#            histo_name=histo_name.replace('CMS_scale_t_3prong','CMS_scale_t_3prong_tt_cat{}'.format(categ))
-
-
-#        histo_name=histo_name.replace('CMS_scale_t_1prong1pizero','CMS_scale_t_1prong1pizero_{}'.format(channel))
-#        histo_name=histo_name.replace('CMS_scale_t_1prongU','CMS_scale_t_1prong_{}U'.format(channel))
-#        histo_name=histo_name.replace('CMS_scale_t_1prongD','CMS_scale_t_1prong_{}D'.format(channel))
-#        histo_name=histo_name.replace('CMS_scale_t_3prong','CMS_scale_t_3prong_{}'.format(channel))
-
-
-#        histo_name=histo_name.replace('CMS_scale_t_1prong1pizero','CMS_scale_t_1prong1pizero_cat{}'.format(categ))
-#        histo_name=histo_name.replace('CMS_scale_t_1prongU','CMS_scale_t_1prong_cat{}U'.format(categ))
-#        histo_name=histo_name.replace('CMS_scale_t_1prongD','CMS_scale_t_1prong_cat{}D'.format(categ))
-#        histo_name=histo_name.replace('CMS_scale_t_3prong','CMS_scale_t_3prong_cat{}'.format(categ))
-
-
-
 
 #        histo_name=histo_name.replace('JEnTot','CMS_scale_j_cat{}'.format(categ))
         histo_name=histo_name.replace('JEnTot','CMS_scale_j')
@@ -351,6 +312,7 @@ for k1 in dirList: # loop over categories
         
         histo_name=histo_name.replace('XH125','XH')
         histo_name=histo_name.replace('ggH125','ggH')
+#        histo_name=histo_name.replace('OutsideAcceptance','OutsideAcceptance125')
         
         
         
