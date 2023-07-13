732b63ae3208216af0e69aeb0a959d8c011cd67d
abdollah110 <smohamadi@gmail.com>
2023-07-13 14:28:43 -0500

    decorrelate the tau Id across categories

--- Limit/ReNamingDC_Limit.py
+++ Limit/ReNamingDC_Limit.py
@@ -264,11 +264,16 @@ for k1 in dirList: # loop over categories
         histo_name=histo_name.replace('_600_800_','_600_800125_')
         histo_name=histo_name.replace('_GT800_','_GT800125_')
         
-        histo_name=histo_name.replace('Bin_1','Bin_1_{}_'.format(lepName))
-        histo_name=histo_name.replace('Bin_2','Bin_2_{}_'.format(lepName))
-        histo_name=histo_name.replace('Bin_3','Bin_3_{}_'.format(lepName))
-        histo_name=histo_name.replace('Bin_4','Bin_4_{}_'.format(lepName))
-        
+#        histo_name=histo_name.replace('Bin_1','Bin_1_{}_'.format(lepName))
+#        histo_name=histo_name.replace('Bin_2','Bin_2_{}_'.format(lepName))
+#        histo_name=histo_name.replace('Bin_3','Bin_3_{}_'.format(lepName))
+#        histo_name=histo_name.replace('Bin_4','Bin_4_{}_'.format(lepName))
+
+        histo_name=histo_name.replace('Bin_1','Bin_1_{}_cat{}_'.format(lepName,categ))
+        histo_name=histo_name.replace('Bin_2','Bin_2_{}_cat{}_'.format(lepName,categ))
+        histo_name=histo_name.replace('Bin_3','Bin_3_{}_cat{}_'.format(lepName,categ))
+        histo_name=histo_name.replace('Bin_4','Bin_4_{}_cat{}_'.format(lepName,categ))
+
         
 #        histo_name=histo_name.replace('ZTT','ZTT125')
         
