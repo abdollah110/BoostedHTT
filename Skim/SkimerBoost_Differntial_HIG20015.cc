b0f331b03f05ca8773833a8945a64a2fbb3ae84f
abdollah <abdollah.mohammadi@cern.ch>
2024-01-29 16:51:20 -0600

    remove all gen cuts add low binning Higgs pT

--- Skim/SkimerBoost_Differntial_HIG20015.cc
+++ Skim/SkimerBoost_Differntial_HIG20015.cc
@@ -71,7 +71,7 @@ void SkimerBoost::Loop(TString OutputFile,std::string InputFile,std::string Sys)
     float eleMass= 0.000511;
 //    float xbin[5]={0,350,450,600,2000};
 //    float xbin_hpt[5]={250,350,450,600,2000};
-    float xbin_hpt[5]={0,45,80,120,200,350,450,600,2000};
+    float xbin_hpt[9]={0,45,80,120,200,350,450,600,2000};
     float xbin_jpt[5]={0,350,450,600,2000};
 //    float xbin[6]={0,350,450,600,800,2000};
 //    float xbin[6]={0,300,400,550,800,2000};
